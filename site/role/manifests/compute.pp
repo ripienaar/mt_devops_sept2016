4772ac4497016984ed553a1f689d6a8e6a998181
R.I.Pienaar <rip@devco.net>
2016-08-29 20:25:48 +0200

    bring mcollective into scope before trying to use it

--- site/role/manifests/compute.pp
+++ site/role/manifests/compute.pp
@@ -2,5 +2,6 @@ class role::compute {
   include profile::consul_agent
   include profile::registrator_server
   include profile::whoami_server
+  include mcollective
   include mcollective_agent_weather
 }
