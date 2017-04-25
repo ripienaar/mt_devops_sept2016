d65eedfb70e1f7b4e1c166336da1eca4f298051d
R.I.Pienaar <rip@devco.net>
2017-04-25 14:30:02 +0200

    do not attempt to setup logentries

--- site/role/manifests/compute.pp
+++ site/role/manifests/compute.pp
@@ -2,7 +2,6 @@ class role::compute {
   include profile::consul_agent
   include profile::registrator_server
   include profile::whoami_server
-  include profile::logentries
   include mcollective
   include mcollective_agent_weather
 }
--- site/role/manifests/traefik.pp
+++ site/role/manifests/traefik.pp
@@ -2,5 +2,4 @@ class role::traefik {
   include profile::consul_server
   include profile::traefik_server
   include profile::registrator_server
-  include profile::logentries
 }
